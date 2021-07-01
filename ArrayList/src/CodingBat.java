8b3664cdd3e9e343d237e61328c6c4164c7d6b29
kbala1 <64056212+kbala1@users.noreply.github.com>
2021-07-01 16:54:35 -0500

    First Commit

---
+++ ArrayList/src/CodingBat.java
@@ -0,0 +1,19 @@
+public class CodingBat {
+	public static void main(String[] args)
+	
+	{	
+		System.out.println("WOrking");
+		System.out.println(old35(3,2, 4));
+	}
+	public static boolean old35(int a, int b, int c )
+	
+	{
+		// return ((b >=a)  && ((c > b) || (c > a)));
+		return ((b >=a)  &&(c>b) || (c >a));
+	}
+}
+
+
+	    
+	   
+
